761a4e533d4cdda9a77e113c03917f3abf5fb59d
Taha Topiwala <taha.topiwala@icloud.com>
2017-07-17 21:15:23 -0700

    Initial Commit

---
+++ Camera Detection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera Detection
+//
+//  Created by Taha Topiwala on 7/17/17.
+//  Copyright © 2017 Taha Topiwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
