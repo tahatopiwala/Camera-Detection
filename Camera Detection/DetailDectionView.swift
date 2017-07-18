c622117f357d7f0d506176065cbc2f5832fcbe07
Taha Topiwala <taha.topiwala@icloud.com>
2017-07-17 23:33:00 -0700

    Removes test target and adds class encapsulations.

---
+++ Camera Detection/DetailDectionView.swift
@@ -0,0 +1,32 @@
+//
+//  DetailDectionView.swift
+//  Camera Detection
+//
+//  Created by Taha Topiwala on 7/17/17.
+//  Copyright © 2017 Taha Topiwala. All rights reserved.
+//
+
+import UIKit
+
+class DetailDectionView: ViewAnimatableAndDragsInYDirectionOnTapGesture {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .lightGray
+        isUserInteractionEnabled = true
+        
+        ViewsOriginalCenter = center
+        ForDragAnimatableInitializeTapGesture()
+    }
+    
+    override var frame: CGRect {
+        didSet {
+            ViewsOriginalCenter = center
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
